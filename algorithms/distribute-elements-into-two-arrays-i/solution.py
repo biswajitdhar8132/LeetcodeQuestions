c800af202181ee275582a5f66267f590020b11fb
Biswajit Dhar <biswajitdhar8132@gmail.com>
2026-08-20 14:06:07 +0530

    feat: add Distribute Elements Into Two Arrays I solution

--- algorithms/distribute-elements-into-two-arrays-i/solution.py
+++ algorithms/distribute-elements-into-two-arrays-i/solution.py
@@ -1,9 +1,15 @@
 class Solution:
-    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        n=len(students)
-        count=[students.count(0), students.count(1)]
-        for sand in sandwiches:
-            if count[sand]==0:
-                break
-            count[sand]-=1
-        return sum(count)
+    def resultArray(self, nums: list[int]) -> list[int]:
+       
+        arr1 = [nums[0]]
+        arr2 = [nums[1]]
+        
+        
+        for i in range(2, len(nums)):
+            if arr1[-1] > arr2[-1]:
+                arr1.append(nums[i])
+            else:
+                arr2.append(nums[i])
+                
+        
+        return arr1 + arr2
