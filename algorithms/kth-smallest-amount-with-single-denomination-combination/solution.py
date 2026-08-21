d6390e0679ed54038047b8e782d4dbe0702d6d1b
Biswajit Dhar <biswajitdhar8132@gmail.com>
2026-08-22 02:06:52 +0530

    feat: add Kth Smallest Amount With Single Denomination Combination solution

--- algorithms/kth-smallest-amount-with-single-denomination-combination/solution.py
+++ algorithms/kth-smallest-amount-with-single-denomination-combination/solution.py
@@ -1,15 +1,53 @@
+import math
+from typing import List
+
 class Solution:
-    def resultArray(self, nums: list[int]) -> list[int]:
-       
-        arr1 = [nums[0]]
-        arr2 = [nums[1]]
+    def findKthSmallest(self, coins: List[int], k: int) -> int:
         
-        
-        for i in range(2, len(nums)):
-            if arr1[-1] > arr2[-1]:
-                arr1.append(nums[i])
-            else:
-                arr2.append(nums[i])
+        coins.sort()
+        filtered_coins = []
+        for c in coins:
+            if not any(c % fc == 0 for fc in filtered_coins):
+                filtered_coins.append(c)
                 
+        n = len(filtered_coins)
+        pie_factors = []
         
-        return arr1 + arr2
+        
+        def precompute_lcm(index, current_lcm, elements_count):
+            if index == n:
+                if elements_count > 0:
+                    sign = 1 if elements_count % 2 == 1 else -1
+                    pie_factors.append((current_lcm, sign))
+                return
+            
+          
+            precompute_lcm(index + 1, current_lcm, elements_count)
+            
+            
+            next_lcm = math.lcm(current_lcm, filtered_coins[index])
+            precompute_lcm(index + 1, next_lcm, elements_count + 1)
+            
+        precompute_lcm(0, 1, 0)
+        
+        def count_valid_amounts(x):
+            count = 0
+            for lcm_val, sign in pie_factors:
+                count += sign * (x // lcm_val)
+            return count
+
+        
+        low = 1
+        high = min(filtered_coins) * k
+        ans = high
+        
+        while low <= high:
+            mid = (low + high) // 2
+            
+            if count_valid_amounts(mid) >= k:
+                ans = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+                
+        return ans
